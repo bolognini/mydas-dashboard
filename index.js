36ce0c60c0a3e1a28e278ded24aa73a745397bc1
Matheus Bolognini <matheusbolognini79@gmail.com>
2018-11-11 16:39:21 -0200

    Add new chart

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-'use strict';
-
 var express = require('express');
 var path = require('path');
 var sql = require("mssql");
 
 var app = express();
+
+const config = {
+  user: 'mydas.manager',
+  password: 'Digital@2018',
+  server: 'mydasmanager.database.windows.net',
+  database: 'mydas-manager',
+  options: {
+    encrypt: true
+  }
+}
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,32 +21,52 @@ app.get('/', function(request, response) {
 });
 
 app.get('/data', function(request, response) {
-
-  const config = {
-    user: 'mydas.manager',
-    password: 'Digital@2018',
-    server: 'mydasmanager.database.windows.net',
-    database: 'mydas-manager',
-    options: {
-      encrypt: true
-    }
-  }
-
   sql.close()
   sql.connect(config, function (err) {
   
-    if (err) console.log(err);
+    if (err) {
+      console.error(err);
+      return
+    }
 
     let request = new sql.Request();
         
-    request.query('select * from dbo.Random', function (err, recordset) {
+    request.query('select * from dbo.Random', function (err, dbresponse) {
         
-      if (err) console.log(err)
-      const value_map = Array.from(recordset.recordset.map(i => i.value))
+      if (err) {
+        console.error(err);
+        return
+      }
+
+      const value_map = Array.from(dbresponse.recordset.map(i => i.value))
 
       response.send(value_map);
-        
     });
   });
 })
+
+app.get('/ram', function(request, response) {
+  sql.close()
+  sql.connect(config, function (err) {
+    if (err) {
+      console.error(err);
+      return
+    }
+
+    let requestRam = new sql.Request();
+
+    requestRam.query('SELECT * FROM dbo.Ram', function (err, dbresponse) {
+
+      if (err) {
+        console.error(err);
+        return
+      }
+      
+      const value_ram = Array.from(dbresponse.recordset.map(i => i.memoryuse))
+
+      response.send(value_ram);
+    })
+  })
+})
+
 app.listen(3000);
--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,33 @@
   'use strict';
 
   let points = [];
+  let pointsRam = [];
   let cnt = 0;
 
   (function getData() {
     $.ajax({
       cache: false,
-      method: "get",
+      method: "GET",
       url: "/data",
     }).done(function(data) {
       points = data;
-      
+    });
+
+    $.ajax({
+      cache: false,
+      method: "GET",
+      url: "/ram",
+    }).done(function(data) {
+      pointsRam = data;
     });
   }())
   
   Plotly.plot('chart', [{
+    y: [0],
+    type: 'line'
+  }]);
+
+  Plotly.plot('chart2', [{
     y: [0],
     type: 'line'
   }]);
@@ -36,5 +49,22 @@
       })
     }
   }, 100);
+
+  setInterval(function() {
+    if(pointsRam.length == 0) {
+      return;
+    }
+
+    Plotly.extendTraces('chart2', { y: [[pointsRam.shift()]] }, [0]);
+    cnt++;
+
+    if(cnt > 30) {
+      Plotly.relayout('chart2', {
+        xaxis: {
+          range: [cnt-30,cnt]
+        }
+      })
+    }
+  }, 100)
 
 })();
